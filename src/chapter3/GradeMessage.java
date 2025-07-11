a00e1431308a9d1a7117ef2b05cb073cdce39209
Sphinx010 <paultreaker@gmail.com>
2025-07-11 18:16:11 +0100

    i removed the former array method^ i replaced String[] message = { } with ArrayList<String> message = new ArrayList<>(), because it is more flexible and dynamic

--- src/chapter3/GradeMessage.java
+++ src/chapter3/GradeMessage.java
@@ -1,5 +1,6 @@
 package chapter3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
@@ -12,40 +13,49 @@ public class GradeMessage {
     public static void main (String[] args){
         //This prints out the instructions required to capture user inputs
         System.out.println("Enter your grade here:");
-        // This captures user inputs from the terminal\console
+        // This captures user inputs
         Scanner scanner = new Scanner(System.in);
-        //This declares a string type variable that stores user inputs captured from the terminal
+        //This declares a string type variable that stores user inputs
         String studentGrade = scanner.next();
-        //This contains an array of Grade messages that would be displayed, with regard to the user inputs
-        String[] message = {"An outstanding performance, well-done!",
-                "A wonderful performance, You can do better!",
-                "An Good performance, You can do better!",
-                "A fair performance, work harder!",
-                "A poor performance, work harder!",
-                "sorry, you did not meet the pass mark!"
-        };
+        //This contains an array of Grade messages that would be displayed
+        ArrayList<String> message = new ArrayList<>();
+        message.add("sorry, you did not meet the pass mark!");
+        message.add("A poor performance, work harder!");
+        message.add("A fair performance, work harder!");
+        message.add("An Good performance, You can do better!");
+        message.add("A wonderful performance, You can do better!");
+        message.add("An outstanding performance, well-done!");
+
+
+                /*******
+                 i removed the former array method:
+                 String[] message = { };
+                 and replaced it a proper "ArrayList method" which is dynamic and flexible
+                 ArrayList<String> message = new ArrayList<>();
+                 **********/
+
         /* Here a switch statement is used,
         it evaluates the expression's value [user input]
         and the case values for equality, and returns its correspondence.
         */
         switch (studentGrade){
             case "F":
-                System.out.println(message[5]);
+                System.out.println(message.get(5));
                 break;
             case "E":
-                System.out.println(message[4]);
+                System.out.println(message.get(4));
                 break;
             case "D":
-                System.out.println(message[3]);
+                System.out.println(message.get(3));
                 break;
             case "C":
-                System.out.println(message[2]);
+                System.out.println(message.get(2));
                 break;
             case "B":
-                System.out.println(message[1]);
+                System.out.println(message.get(1));
                 break;
             case "A":
-                System.out.println(message[0]);
+                System.out.println(message.get(0));
                 break;
             default:
                 System.out.println("Student grades are from " + "A" + " through " + "F");
